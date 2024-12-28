9162c475deba0c6fb53ec8b6276cfdf1a236fcdc
Liu Yuhe <171144077+liuyuhe666@users.noreply.github.com>
2024-12-28 15:30:46 +0800

    feat: npm publish

---
+++ version.py
@@ -0,0 +1,11 @@
+import json
+
+with open('package.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+current_version = data['version']
+major, minor, patch = map(int, current_version.split('.'))
+patch += 1
+new_version = f'{major}.{minor}.{patch}'
+data['version'] = new_version
+with open('package.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=2)
